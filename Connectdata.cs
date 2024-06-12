1c4cc7c928842839e78096fcda3016e949135875
Thanh Dat <tthanhdat.pb@gmail.com>
2024-06-12 14:45:49 +0700

    Add project files.

---
+++ Connectdata.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace sqltest
+{
+    class ConnectData
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+
+                builder.DataSource = "<Sqlserver.database.windows.net>";
+                builder.UserID = "<sa>";
+                builder.Password = "<Tranthanhdat110603@>";
+                builder.InitialCatalog = "<RazorPagesMovie>";
+
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    Console.WriteLine("\nQuery data example:");
+                    Console.WriteLine("=========================================\n");
+
+                    connection.Open();
+
+                    String sql = "SELECT name, collation_name FROM sys.databases";
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Console.WriteLine("{0} {1}", reader.GetString(0), reader.GetString(1));
+                            }
+                        }
+                    }
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.WriteLine("\nDone. Press enter.");
+            Console.ReadLine();
+        }
+    }
+}
